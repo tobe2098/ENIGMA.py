eed48fd67fe78b0ab605435a1cc5fca33bea4ff4
tobe2098 <toni.rampage@jabertolin.me>
2022-05-23 04:52:55 +0100

    1.0.1: Added editing phase to EXEC.py

--- EXEC.py
+++ EXEC.py
@@ -24,4 +24,9 @@ if input("Do you want to use a saved machine?[y/n]").lower()=="y":
 else:
     machine.random_machine()
     new_name=input("Put a name to your new ENIGMA machine so it is easier to ID afterwards. \nDo NOT put a overly revealing name related to your communications.")
+    machine.change_name(new_name)
+    machine.save_machine()
+    print("Delete the randomly generated pickle file, keep the named one for you")
+    if input("Do you want to do an adjustment on the randomly generated machine?[y/n]:")=="y":
+        machine.manual_complete_config()
 machine.encrypt_decrypt()
