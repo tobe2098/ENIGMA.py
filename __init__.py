69264998cf419b33b79782c277a2c44eef65a51b
Antoni Bertolin <tonibm1220@gmail.com>
2024-03-01 19:01:10 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,6 +6,7 @@ import gui
 ## ADD MAx DEPTH for recursive calls
 ## Add configuration of bools for safety and clarity screen clearance, MAX NOTCHES (configuration.something?)
 # Move constants somewhere that is not utils? If I can make them static somehow (or constants.py file)
+# Introduce Type_sth class for string printing? Lowecase, uppercase, plural and singular.
 
 
 if __name__ == "__main__":
